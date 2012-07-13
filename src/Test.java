e45e961e11c697043512a6dc0f2cd4cffcc7d5ae
maryamaidini <maidini@thoughtworks.com>
2012-07-13 11:36:10 +0100

    test

---
+++ src/Test.java
@@ -0,0 +1,16 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: maidini
+ * Date: 13/07/2012
+ * Time: 10:15
+ * To change this template use File | Settings | File Templates.
+ */
+public class Test {
+
+         public static void main(String[] args)
+         {
+                 System.out.println("Hello World");
+         }
+
+
+}
