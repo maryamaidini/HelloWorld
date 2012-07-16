d6c7d5cb177bf4ee4b94065b2a75de0b35fc6feb
maidini <maidini@thoughtWorks.com>
2012-07-16 11:50:20 +0100

    test

--- src/Test.java
+++ src/Test.java
@@ -10,6 +10,7 @@ public class Test {
          public static void main(String[] args)
          {
                  System.out.println("Hello World");
+             System.out.println("Hello git");
          }
 
 
